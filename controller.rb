8baa4f6dce9bdd36a66e689492a8bccefd88e63f
Carlo Cabanilla <ccabanilla@wgen.net>
2010-05-30 13:13:24 -0400

    implement basic add and remove services

--- controller.rb
+++ controller.rb
@@ -4,15 +4,51 @@
 # Created by Carlo Cabanilla on 5/30/10.
 # Copyright 2010 Carlo Cabanilla. All rights reserved.
 
+class Service
+  def initialize(name)
+    @name = name
+  end
+  
+  def to_s
+    @name
+  end
+end
+
 class Cephalopod
   attr_writer :servicesView, :logOutputView
   
+  def awakeFromNib
+    @services = []
+    @servicesView.dataSource = self
+  end
+  
+  # servicesView protocol implementation
+  
+  def numberOfRowsInTableView(view)
+    @services.size
+  end
+
+  def tableView(view, objectValueForTableColumn:column, row:index)
+    @services[index].to_s
+  end  
+  
+  # Actions
+  
   def addService(sender)
     NSLog('add service')
+    service = Service.new 'Test service'
+    @services << service
+    @servicesView.reloadData
   end
 
   def deleteService(sender)
-    NSLog('delete service')
+    row_index = @servicesView.selectedRow
+    
+    if row_index > -1 then
+      NSLog('delete service')
+      @services.delete_at row_index
+      @servicesView.reloadData
+    end
   end
 
   def startService(sender)
