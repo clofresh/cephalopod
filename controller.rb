645423bdbeb9fbcc37ee5063a0dcca5580fe27a1
Carlo Cabanilla <ccabanilla@wgen.net>
2010-05-30 14:20:36 -0400

    read from a file and append to textview

--- controller.rb
+++ controller.rb
@@ -4,9 +4,13 @@
 # Created by Carlo Cabanilla on 5/30/10.
 # Copyright 2010 Carlo Cabanilla. All rights reserved.
 
+framework 'Cocoa'
+require 'dispatch'
+
 class Service
-  def initialize(name)
+  def initialize(name, script)
     @name = name
+    @script = script
   end
   
   def to_s
@@ -18,6 +22,7 @@ class Cephalopod
   attr_writer :servicesView, :logOutputView
   
   def awakeFromNib
+    @queue = Dispatch::Queue.new 'com.cephalopodapp.services'
     @services = []
     @servicesView.dataSource = self
   end
@@ -36,7 +41,7 @@ class Cephalopod
   
   def addService(sender)
     NSLog('add service')
-    service = Service.new 'Test service'
+    service = Service.new 'Test service', 'tail -f /var/log/system.log'
     @services << service
     @servicesView.reloadData
   end
@@ -52,7 +57,24 @@ class Cephalopod
   end
 
   def startService(sender)
-    NSLog('start service')
+    row_index = @servicesView.selectedRow
+    
+    if row_index > -1 or true then
+      NSLog('start service')
+      file = File.open('/tmp/input.log')
+      
+      Dispatch::Source.read(file, @queue) do |s|
+        data = file.read(s.data)
+        
+        endRange = NSRange.new
+        endRange.location = @logOutputView.textStorage.length
+        endRange.length = 0
+        @logOutputView.replaceCharactersInRange(endRange, withString:data)
+        endRange.length = data.length
+        @logOutputView.scrollRangeToVisible(endRange)
+        NSLog("Got data: #{data}")
+      end
+    end
   end
 
   def stopService(sender)
